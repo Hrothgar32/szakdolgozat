0cbcf045faf44f4e07992fcdcd28c60831b1068d
hrothgar32 <zold.almos@gmail.com>
2023-03-06 19:19:07 +0200

    Elso commit

---
+++ .auctex-auto/dolgozat.el
@@ -0,0 +1,35 @@
+(TeX-add-style-hook
+ "dolgozat"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "definitions"
+    "biblatex")
+   (LaTeX-add-labels
+    "sec:org68ba897"
+    "sec:orga83fc37"
+    "sec:org01c66eb")
+   (LaTeX-add-bibliographies
+    "/home/hrothgar32/Documents/allamvizsga/allamvizsga"))
+ :latex)
+
---
+++ progfiles/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Print a hello world message
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    // print hello to the console
+    System.out.println("Hello, World!");
+  }
+}
