a95e6e3c17da7905cea4223e8b9cf85cae1f2265
hrothgar32 <zold.almos@gmail.com>
2023-04-12 22:17:25 +0300

    Slope one behozás, LSH nagyjából megvan

--- .auctex-auto/dolgozat.el
+++ .auctex-auto/dolgozat.el
@@ -3,13 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "inputenc"
@@ -26,16 +27,17 @@
     "minted"
     "natbib")
    (LaTeX-add-labels
-    "sec:org5136892"
-    "sec:org8c47bb4"
-    "sec:org15d5cf3"
-    "sec:org164b5cb"
-    "sec:orgc57cbe3"
-    "sec:org3fb8753"
-    "sec:org91f5f97"
-    "sec:org92a14c3"
-    "sec:orge4d1368"
-    "sec:org27215df")
+    "sec:org41c75a4"
+    "sec:orgfefd3a7"
+    "sec:org55df85d"
+    "sec:org37134da"
+    "sec:org526be0e"
+    "sec:orgdca9e20"
+    "sec:orgee7fbcf"
+    "sec:org24ae2e5"
+    "sec:orgd2c28bf"
+    "sec:org5406cac"
+    "sec:orgce5f470")
    (LaTeX-add-bibliographies
     "/home/hrothgar32/Documents/Egyetem/Allamvizsga/Dolgozat/allamvizsga"
     "/home/hrothgar32/Documents/allamvizsga/allamvizsga"))
